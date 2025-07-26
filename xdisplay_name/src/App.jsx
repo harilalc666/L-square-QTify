1d81236d90d7c40c28e7c0b876bd66363dfaf5b8
harilal.chauhan <harilal.chauhan>
2025-07-26 11:58:26 +0530

    completed xDisplayName

---
+++ xdisplay_name/src/App.jsx
@@ -0,0 +1,53 @@
+import { useState } from "react";
+
+function App() {
+  const [fullName, setFullName] = useState({ firstName: "", lastName: "" });
+  const [isFormSubmit, setFormSubmit] = useState(false);
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    setFormSubmit(true);
+  };
+
+  return (
+    <div>
+      <form
+        onSubmit={handleFormSubmit}
+        style={{ display: "flex", flexDirection: "column" }}
+      >
+        <label htmlFor="">
+          First Name:  
+          <input
+            type="text"
+            value={fullName.firstName}
+            onChange={(e) =>
+              setFullName({ ...fullName, firstName: e.target.value })
+            }
+            required
+          />
+        </label>
+
+        <label htmlFor="">
+          Last Name: 
+          <input
+            type="text"
+            value={fullName.lastName}
+            onChange={(e) =>
+              setFullName({ ...fullName, lastName: e.target.value })
+            }
+            required
+          />
+        </label>
+        <button type="submit" style={{ width: "100px", marginTop: "10px" }}>
+          submit
+        </button>
+      </form>
+
+      <p>
+        {isFormSubmit && `Full Name: ${fullName.firstName} ${fullName.lastName}`}
+      </p>
+    </div>
+  );
+}
+
+export default App;
